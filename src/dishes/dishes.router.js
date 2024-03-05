a3118957126b87441651a5621d21c1d88bb0df3c
Ahmad Abusaif <129558794+aabusaif8@users.noreply.github.com>
2024-03-04 19:33:39 -0600

    Updating Router

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-
+const methodNotAllowed = require("../errors/methodNotAllowed")
 const controller = require("./dishes.controller")
 
-router.route("/:dishId").get(controller.read).delete(controller.delete).put(controller.update)
+router.route("/:dishId").get(controller.read).delete(controller.delete).put(controller.update).all(methodNotAllowed)
 
-router.route("/").get(controller.list).post(controller.create)
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed)
 
 module.exports = router;
