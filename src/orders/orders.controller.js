5c5042ef3eb9ac39f1846b1285fe2774535f68b1
Ahmad Abusaif <129558794+aabusaif8@users.noreply.github.com>
2024-03-04 19:18:27 -0600

    updating ordercontroller

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -1,81 +1,76 @@
 const path = require("path");
-
-// Use the existing order data
 const orders = require(path.resolve("src/data/orders-data"));
-
-// Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
-function list(req,res){
-  res.send({data:orders})
+function list(req, res) {
+  res.send({ data: orders });
 }
 
+function idChecker(req, res, next) {
+  const orderId = req.params.orderId;
+  const dataId = req.body.data.id;
 
-function idChecker(req,res,next){
-  const orderId = req.params.orderId
-  const dataId = req.body.data.id
-  
-  if(!orderId){
+  if (!orderId) {
     return next({
-      status:400,
-      message:`${dataId}`
-    })
-  } 
-  if(dataId && orderId !== dataId ){
+      status: 400,
+      message: `${dataId}`,
+    });
+  }
+  if (dataId && orderId !== dataId) {
     return next({
-      status:400,
-      message:`${dataId} id`
-    })
+      status: 400,
+      message: `${dataId} id`,
+    });
   }
-    next()
+  next();
 }
 
-
-function orderExists(req,res,next){
-  const orderId = req.params.orderId
-  const foundOrder = orders.find((order) => order.id === orderId)
-  if(!foundOrder){
+function orderExists(req, res, next) {
+  const orderId = req.params.orderId;
+  const foundOrder = orders.find((order) => order.id === orderId);
+  if (!foundOrder) {
     return next({
-      status:404,
-      message:`${orderId}`
-    })
+      status: 404,
+      message: `${orderId}`,
+    });
   }
-  next()
-}
-function deliverChecker(req,res,next){
-  const deliverTo = req.body.data.deliverTo
-  if (!deliverTo || deliverTo === ""){
-    next({
-      status:400,
-      message:"deliverTo"
-    })
-  }
-  next()
+  res.locals.foundOrder = foundOrder;
+  next();
 }
 
-function mobileNumberChecker(req,res,next){
-  const mobileNumber = req.body.data.mobileNumber
-  
-  if(!mobileNumber || mobileNumber === ""){
+function deliverChecker(req, res, next) {
+  const deliverTo = req.body.data.deliverTo;
+  if (!deliverTo || deliverTo === "") {
     next({
-      status:400,
-      message:"mobileNumber"
-    })
+      status: 400,
+      message: "deliverTo",
+    });
   }
-  next()
+  next();
 }
 
-function dishesChecker(req,res,next){
-  const dishes = req.body.data.dishes
-  
-  if(!dishes || dishes === ""|| dishes.length === 0|| !Array.isArray(dishes)){
+function mobileNumberChecker(req, res, next) {
+  const mobileNumber = req.body.data.mobileNumber;
+
+  if (!mobileNumber || mobileNumber === "") {
     next({
-      status:400,
-      message:"dishes missing or empty"
-    })
+      status: 400,
+      message: "mobileNumber",
+    });
   }
-  next()
+  next();
+}
+
+function dishesChecker(req, res, next) {
+  const dishes = req.body.data.dishes;
+
+  if (!dishes || dishes === "" || dishes.length === 0 || !Array.isArray(dishes)) {
+    next({
+      status: 400,
+      message: "dishes missing or empty",
+    });
+  }
+  next();
 }
 
 function quantityChecker(req, res, next) {
@@ -88,83 +83,81 @@ function quantityChecker(req, res, next) {
   }
 
   for (const index in dishes) {
-    let dish = dishes[index]
-    const quantity  = dish.quantity
-    if(!(quantity) || quantity < 1 || !(Number.isInteger(quantity))){
+    let dish = dishes[index];
+    const quantity = dish.quantity;
+    if (!(quantity) || quantity < 1 || !(Number.isInteger(quantity))) {
       return next({
-        status:400,
-        message: `Dish ${index} must have a quantity that is an integer greater than 0`
-      })
+        status: 400,
+        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+      });
     }
   }
   next();
 }
 
+function statusChecker(req, res, next) {
+  const orderId = req.params.orderId;
+  const foundOrder = orders.find((order) => order.id === orderId);
 
-function statusChecker(req,res,next){
-  const orderId = req.params.orderId
-  const foundOrder = orders.find((order) => order.id === orderId)
-  
-  const orderStatus = foundOrder.status
-  console.log(orderStatus)
-  if(!orderStatus|| orderStatus === ""){
+  const orderStatus = foundOrder.status;
+  if (!orderStatus || orderStatus === "") {
     return next({
-      status:400,
-      message:'status'
-    })
+      status: 400,
+      message: 'status',
+    });
   }
-  if(orderStatus !== 'pending'){
+  if (orderStatus !== 'pending') {
     return next({
-      status:400,
-      message:'pending'
-    })
+      status: 400,
+      message: 'pending',
+    });
   }
-  next()
+  next();
 }
 
-function read(req,res,next){
-  const orderId = req.params.orderId
-  const foundOrder = orders.find((order) => order.id === orderId)
-   
-  if(foundOrder){
-    return res.send({data:foundOrder})
+function read(req, res, next) {
+  const foundOrder = res.locals.foundOrder;
+  if (foundOrder) {
+    return res.send({ data: foundOrder });
   }
   next({
-    status:404,
-    message:"Invalid orderId"
-  })
+    status: 404,
+    message: "Invalid orderId",
+  });
 }
-function orderStatus(req,res,next){
-  const { data : { status } = { } } = req.body
+
+function orderStatus(req, res, next) {
+  const { data: { status } = {} } = req.body;
   if (!status || typeof status !== 'string' || status.trim().length === 0) {
     return next({
       status: 400,
-      message: 'Order should have a non-empty status.'
+      message: 'Order should have a non-empty status.',
     });
   }
 
-  const validStatusValues = ['pending','processing','completed']
-  if(!validStatusValues.includes(status.toLowerCase())){
+  const validStatusValues = ['pending', 'processing', 'completed'];
+  if (!validStatusValues.includes(status.toLowerCase())) {
     return next({
-      status:400,
-      message:'status'
-    })
+      status: 400,
+      message: 'status',
+    });
   }
-  next()
+  next();
 }
-function destroy(req,res,next){
-  const orderId = req.params.orderId
-  const foundOrder = orders.find((order) => order.id === orderId)
-  const index = orders.findIndex((order) => order.id === orderId)
-  const orderStatus = foundOrder.status
-  if( index > -1){
-    orders.splice(index, 1)
-    res.sendStatus(204)
+
+function destroy(req, res, next) {
+  const orderId = req.params.orderId;
+  const foundOrder = orders.find((order) => order.id === orderId);
+  const index = orders.findIndex((order) => order.id === orderId);
+  const orderStatus = foundOrder.status;
+  if (index > -1) {
+    orders.splice(index, 1);
+    res.sendStatus(204);
   }
   next({
-    status:404,
-    message:`${orderId}`
-  })
+    status: 404,
+    message: `${orderId}`,
+  });
 }
 
 function update(req, res, next) {
@@ -172,7 +165,6 @@ function update(req, res, next) {
   const newOrder = req.body.data;
 
   const index = orders.findIndex((order) => Number(order.id) === Number(orderId));
-
   orders[index] = {
     ...orders[index],
     ...newOrder,
@@ -194,7 +186,7 @@ function create(req,res,next){
 
 module.exports = {
   list,
-  read,
+  read: [orderExists,read],
   create: [deliverChecker,mobileNumberChecker,quantityChecker,dishesChecker,create],
   update: [orderExists,idChecker,orderStatus,deliverChecker,mobileNumberChecker,dishesChecker,quantityChecker,statusChecker,update],
   delete:[orderExists,statusChecker,destroy],
